3ff48dcbef835aecb190bdf2af5f46452bdbb8ff
AlexTrinityBLock <q123717111@gmail.com>
2022-12-24 04:08:23 +0000

    開發Publisher

--- codes/Publisher/Message.py
+++ codes/Publisher/Message.py
@@ -1,4 +1,6 @@
 import json
+from gpiozero import CPUTemperature
+import psutil
 
 class Message:
     def __init__(self):
@@ -6,5 +8,11 @@ class Message:
         self.message['From'] = 'Publisher'
         
     def get(self):
-        text = json.dumps(self.message)
-        return text
+        cpu = CPUTemperature()
+        plain_text_message = dict()
+        plain_text_message['CPU_Temperature'] = cpu.temperature
+        plain_text_message['CPU_Usage'] = psutil.cpu_percent()
+        plain_text_message['RAM_Usage'] = psutil.virtual_memory().percent
+        cipher_text = json.dumps(plain_text_message)
+        self.message['Cipher_Text'] = cipher_text        
+        return json.dumps(self.message)
