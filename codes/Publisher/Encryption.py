3f2c7318c247a78a24c4e625bb87a56158fc60c0
AlexTrinityBLock <q123717111@gmail.com>
2022-12-24 14:54:45 +0000

    更新

--- codes/Publisher/Encryption.py
+++ codes/Publisher/Encryption.py
@@ -4,9 +4,43 @@ from charm.toolbox.ABEncMultiAuth import ABEncMultiAuth
 from charm.schemes.abenc.abenc_dacmacs_yj14 import DACMACS
 from charm.core.engine.util import objectToBytes,bytesToObject
 from StringEncode import StringEncode
+from base64 import b64encode,b64decode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 import json
+import hashlib
 
 class Encryption:
+
+    def AES_encrypt(self,message:str,key:str):
+        message = message.encode("utf-8")
+        key = key.encode("utf-8")
+
+        shavalue = hashlib.sha256()
+        shavalue.update(key)
+        key= shavalue.digest()
+
+        cipher = AES.new(key, AES.MODE_CTR)
+        ct_bytes = cipher.encrypt(message)
+        nonce = b64encode(cipher.nonce).decode('utf-8')
+        ct = b64encode(ct_bytes).decode('utf-8')
+        result = json.dumps({'nonce':nonce, 'ciphertext':ct})
+        return result 
+
+    def AES_decrypt(self,cipher_text:str,key:str):
+        key = key.encode("utf-8")
+        shavalue = hashlib.sha256()
+        shavalue.update(key)
+        key= shavalue.digest()
+        try:
+            b64 = json.loads(cipher_text)
+            nonce = b64decode(b64['nonce'])
+            ct = b64decode(b64['ciphertext'])
+            cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+            pt = cipher.decrypt(ct)
+            return pt.decode('utf-8')
+        except (ValueError, KeyError):
+            print("Incorrect decryption")
 
     def encrypt(self,message:str):
         string_encode = StringEncode()
@@ -40,3 +74,10 @@ class Encryption:
         cipher_AES_key = objectToBytes(CT, PairingGroup('SS512')).decode("utf-8")
 
         return cipher_AES_key
+
+if __name__ == '__main__':
+    encryption = Encryption()
+    cipher = encryption.AES_encrypt('咕咕雞','馬克思直呼內行')
+    print(cipher)
+    result = encryption.AES_decrypt(cipher,'馬克思直呼內行')
+    print("解密後",result)
--- examples/crypto-example.py
+++ examples/crypto-example.py
@@ -4,19 +4,27 @@ from charm.toolbox.ABEncMultiAuth import ABEncMultiAuth
 from charm.schemes.abenc.abenc_dacmacs_yj14 import DACMACS
 
 print("RUN revokedTest")
+# 設置群
 groupObj = PairingGroup('SS512')
+# 實體化物件
 dac = DACMACS(groupObj)
+# 設置公共參數
 GPP, GMK = dac.setup()
 
 users = {} # public user data
 authorities = {}
 
+# 建立屬性
 authorityAttributes = ["ONE", "TWO", "THREE", "FOUR"]
+# 建立授權角色
 authority1 = "authority1"
 
+# 設置授權角色
 dac.setupAuthority(GPP, authority1, authorityAttributes, authorities)
 
+# 設置使用者
 alice = { 'id': 'alice', 'authoritySecretKeys': {}, 'keys': None }
+# 設置使用者鑰匙兩把鑰匙
 alice['keys'], users[alice['id']] = dac.registerUser(GPP)
 
 # print("Alice 的 Keys",alice['keys'])
@@ -25,21 +33,28 @@ alice['keys'], users[alice['id']] = dac.registerUser(GPP)
 bob = { 'id': 'bob', 'authoritySecretKeys': {}, 'keys': None }
 bob['keys'], users[bob['id']] = dac.registerUser(GPP)
 
+# 設置使用者的authority SecretKey 另外一把鑰匙
 for attr in authorityAttributes[0:-1]:
     dac.keygen(GPP, authorities[authority1], attr, users[alice['id']], alice['authoritySecretKeys'])
     dac.keygen(GPP, authorities[authority1], attr, users[bob['id']], bob['authoritySecretKeys'])
 
+# 生成明文，其實是一把加密用的 AES Key ，因為該數值只能在GT群中。
 k = groupObj.random(GT)
 
+#　制定政策
 policy_str = '((ONE or THREE) and (TWO or FOUR))'
 
+# 加密後的 Key
 CT = dac.encrypt(GPP, policy_str, k, authorities[authority1])
 
+# Alice 的第一步解密 (位於委外設施)
 TK1a = dac.generateTK(GPP, CT, alice['authoritySecretKeys'], alice['keys'][0])
+# Alice 的第二步解密 (Alice 本人解密)
 PT1a = dac.decrypt(CT, TK1a, alice['keys'][1])
 TK1b = dac.generateTK(GPP, CT, bob['authoritySecretKeys'], bob['keys'][0])
 PT1b = dac.decrypt(CT, TK1b, bob['keys'][1])
 
+# 檢查 AES Key 解密後，是否與解密前相同。
 assert k == PT1a, 'FAILED DECRYPTION (1a)!'
 assert k == PT1b, 'FAILED DECRYPTION (1b)!'
 print('SUCCESSFUL DECRYPTION 1')
