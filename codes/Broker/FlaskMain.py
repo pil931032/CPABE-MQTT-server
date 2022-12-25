2da35b3e9957b31478a1eb8de21809bc55f88165
AlexTrinityBlock <q123717111@gmail.com>
2022-12-25 21:11:31 +0800

    更新

---
+++ codes/Broker/FlaskMain.py
@@ -0,0 +1,44 @@
+from flask import Flask,request
+from flask_cors import CORS, cross_origin
+import yaml
+import json
+from OpenSSL import SSL
+
+# OpenSSL
+context = SSL.Context(SSL.TLSv1_2_METHOD)
+context.use_privatekey_file('ca.key')
+context.use_certificate_file('ca.crt') 
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Load keys
+def load_keys():
+    with open('keys.yaml', 'r') as f:
+        return yaml.safe_load(f)
+
+# Load users_passwords
+def load_users_passwords():
+    with open('user_password.yaml', 'r') as f:
+        return yaml.safe_load(f)
+
+# Send Global Parameter
+@app.route("/global-parameters/<user>/<password>", methods=['GET', 'POST'])
+@cross_origin()
+def global_parameters(user,password):
+  # Example http://10.1.0.1/global-parameters/Alice/abc123s
+  # Verify Auth
+  user_password = load_users_passwords()
+  try:
+    if user_password[user] != password:
+      return {'code':0}
+  except:
+    return {'code':0}
+  # Send Global Parameters
+  global_parameters = dict()
+  keys = load_keys()
+  global_parameters['GPP'] = keys['GPP']
+  global_parameters['authority'] = keys['authority']
+  global_parameters:str = json.dumps(global_parameters)
+  return global_parameters
