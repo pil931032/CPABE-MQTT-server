75d27471985551910d25ca4dfa4a265c3983d40a
AlexTrinityBLock <q123717111@gmail.com>
2022-12-27 11:57:25 +0000

    更新

--- codes/Publisher/main.py
+++ codes/Publisher/main.py
@@ -5,6 +5,8 @@ import time
 from Message import Message
 from amqtt.client import MQTTClient
 from amqtt.mqtt.constants import QOS_0, QOS_1, QOS_2
+from Render import Render
+import json
 
 # logger = logging.getLogger(__name__)
 
@@ -18,15 +20,19 @@ async def main_loop(setting):
     while True:
         message = Message()
         message_text = message.get()
-        # print(message_text)
+        message_object = json.loads(message_text)
+        # Rende Table
+        render = Render()
+        render.table()
+        # MQTT send
         MQTT_client = MQTTClient()
         await MQTT_client.connect('mqtt://'+setting['BrockerIP']+'/')
         tasks = [
             asyncio.ensure_future(MQTT_client.publish('message/public', message_text.encode(encoding='utf-8'), qos=QOS_2)),
         ]
         time.sleep(int(setting['IntervelTimeSecond']))
-        await asyncio.wait(tasks)
-        await MQTT_client.disconnect()
+    await asyncio.wait(tasks)
+    await MQTT_client.disconnect()
 
 # Main function
 if __name__ == '__main__':
