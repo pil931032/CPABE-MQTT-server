7abcafc812367662d1925c4326a672d888912221
AlexTrinityBlock <q123717111@gmail.com>
2022-12-29 08:39:46 +0800

    更新

--- codes/Subscriber/main.py
+++ codes/Subscriber/main.py
@@ -31,15 +31,16 @@ async def uptime_coro():
             message_text = str(packet.payload.data,encoding='utf-8')
             # print("%d:  %s => %s" % (i, packet.variable_header.topic_name, message_text))
             message_obj = json.loads(message_text)
-            # Time-consuming calculation
-            start_time = datetime.datetime.fromtimestamp(message_obj['UTC-Time'])
-            receive_time = datetime.datetime.now()
-            time_consuming_string = str((receive_time - start_time).total_seconds())
             # Cipher Text
             Cipher_AES_Key = message_obj['Cipher_AES_Key']
             Cipher_Text = message_obj['Cipher_Text']
             decryption = Decryption()
             plain_text, user_attribute= decryption.decryption(Cipher_AES_Key,Cipher_Text)
+            # Time-consuming calculation
+            start_time = datetime.datetime.fromtimestamp(message_obj['UTC-Time'])
+            receive_time = datetime.datetime.now()
+            time_consuming_string = str((receive_time - start_time).total_seconds())
+            # Render
             result = json.loads(plain_text)
             render = Render()
             render.table(
